bdc208b3063048370aecb1fce7a3d4a262a56a81
EngBett <bettthetimetraveller@gmail.com>
2020-06-30 15:08:15 +0300

    New classes

--- AsynPrograming/TestClass.cs
+++ AsynPrograming/TestClass.cs
@@ -1,7 +1,25 @@
-﻿namespace AsynPrograming
+﻿using System;
+
+namespace AsynPrograming
 {
     public class TestClass
     {
-        
+        public TestClass()
+        {
+            
+        }
+
+        public void GuidLoop()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine(new Guid().ToString());
+            }
+        }
+
+        public void AnotherLoop()
+        {
+            
+        }
     }
 }
